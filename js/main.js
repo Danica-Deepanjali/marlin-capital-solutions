c5ae10662b09efa69be3fec1d045d0e0eba556bf
Danica Deepanjali <danica.deepanjali@ithands.biz>
2019-10-04 17:03:25 +0530

    added source files

---
+++ js/main.js
@@ -0,0 +1,77 @@
+jQuery(function($) {
+  $(document).ready(function() {
+    $("#getStarted,#revealcontent").click(function() {
+      $("body").toggleClass("fadeOut");
+    });
+
+    // smooth scrolling
+    $(document).on("click", "a[href*=letsTalk]", function(event) {
+      if ($(this.hash).length) {
+        $("html, body").animate(
+          {
+            scrollTop: $(this.hash).offset().top - 100
+          },
+          500
+        );
+      }
+    });
+  });
+
+
+  // scroll on laptops
+
+  // $(window).on('mousewheel DOMMouseScroll', function (e) {
+  //   $("body").toggleClass('fadeOut');
+  //   //prevent page fom scrolling
+  //   return false;
+  // });
+
+  // checks if the specified event is supported by the browser.
+  function isEventSupported(eventName) {
+    var el = document.createElement('div');
+    eventName = 'on' + eventName;
+    var isSupported = (eventName in el);
+    if (!isSupported) {
+      el.setAttribute(eventName, 'return;');
+      isSupported = typeof el[eventName] == 'function';
+    }
+    el = null;
+    return isSupported;
+  }
+
+  // in browsers where both events are supported.
+  var wheelEvent = isEventSupported('mousewheel') ? 'mousewheel' : 'wheel';
+
+  // Now bind the event to the desired element
+  $('body').on(wheelEvent, function (e) {
+    var oEvent = e.originalEvent,
+      delta = oEvent.deltaY || oEvent.wheelDelta;
+    // deltaY for wheel event
+    // wheelData for mousewheel event
+    if (delta > 0) {
+      $("body").addClass('fadeOut');
+    } else {
+      $("body").removeClass('fadeOut');
+    }
+  });
+
+  // tabs to accordion
+  $(window)
+    .on("resize", function() {
+      if ($(window).width() < 992) {
+        $(".tab-content .tab-pane").removeClass("fade");
+        $(".tab-content .tab-pane").on("click", function() {
+          var container = $(this).parents(".tabbed-content"),
+            currId = $(this).attr("id"),
+            items = container.find(".tab-pane");
+          container.find(".nav-tabs a").removeClass("active");
+          items.removeClass("active");
+          $(this).addClass("active");
+          container
+            .find('.nav-tabs a[href$="#' + currId + '"]')
+            .addClass("active");
+        });
+      }
+    })
+    .resize();
+});

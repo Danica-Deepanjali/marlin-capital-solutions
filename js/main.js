dfb45d03e6792bc0a0a35c06c4d9d9013cc5f3a6
Danica Deepanjali <danica.deepanjali@ithands.biz>
2019-10-10 15:07:46 +0530

    removed faulty js

--- js/main.js
+++ js/main.js
@@ -76,29 +76,29 @@ jQuery(function ($) {
         });
 
         // // smooth scrolling on mobile
-        $('a[href="#letsTalk"]')
-          .click(function (event) {
-            if (
-              location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-              &&
-              location.hostname == this.hostname
-            ) {
-              // Figure out element to scroll to
-              var target = $(this.hash);
-              target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-              // Does a scroll target exist?
-              if (target.length) {
-                // Only prevent default if animation is actually gonna happen
-                event.preventDefault();
+        // $('a[href="#letsTalk"]')
+        //   .click(function (event) {
+        //     if (
+        //       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+        //       &&
+        //       location.hostname == this.hostname
+        //     ) {
+        //       // Figure out element to scroll to
+        //       var target = $(this.hash);
+        //       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        //       // Does a scroll target exist?
+        //       if (target.length) {
+        //         // Only prevent default if animation is actually gonna happen
+        //         event.preventDefault();
 
-                var scrollTop = target.offset().top - $('.sticky-top').height() - 100;
+        //         var scrollTop = target.offset().top - $('.sticky-top').height() - 100;
 
-                $('html, body').animate({
-                  scrollTop: scrollTop
-                }, 1000)
-              }
-            }
-          });
+        //         $('html, body').animate({
+        //           scrollTop: scrollTop
+        //         }, 1000)
+        //       }
+        //     }
+        //   });
       }
     })
     .resize();

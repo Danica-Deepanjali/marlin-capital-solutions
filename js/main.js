e2fb8224fa35cecc8bf176fcbda33c47030c6203
Danica Deepanjali <danica.deepanjali@ithands.biz>
2019-10-10 15:51:51 +0530

    mobile smooth scroll issue

--- js/main.js
+++ js/main.js
@@ -96,7 +96,7 @@ jQuery(function ($) {
                 // Only prevent default if animation is actually gonna happen
                 event.preventDefault();
                 $('html, body').animate({
-                  scrollTop: target.offset().top
+                  scrollTop: target.offset().top - 120
                 }, 1000, function () {
                   // Callback after animation
                   // Must change focus!

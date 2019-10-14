68f7bf244df422227913b2e3cc238d85ba596ebc
Danica Deepanjali <danica.deepanjali@ithands.biz>
2019-10-14 10:18:01 +0530

    added new favicon and updated get started links

--- js/main.js
+++ js/main.js
@@ -6,50 +6,6 @@ jQuery(function ($) {
     });
   });
 
-  // // smooth scrolling on mobile
-  $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]')
-    .not('[href="#0"]')
-    .click(function (event) {
-      // On-page links
-      if (
-        location.pathname.replace(/^\//, "") ==
-        this.pathname.replace(/^\//, "") &&
-        location.hostname == this.hostname
-      ) {
-        // Figure out element to scroll to
-        var target = $(this.hash);
-        target = target.length
-          ? target
-          : $("[name=" + this.hash.slice(1) + "]");
-        // Does a scroll target exist?
-        if (target.length) {
-          // Only prevent default if animation is actually gonna happen
-          event.preventDefault();
-          $("html, body").animate(
-            {
-              scrollTop: target.offset().top - 120
-            },
-            500,
-            function () {
-              // Callback after animation
-              // Must change focus!
-              var $target = $(target);
-              $target.focus();
-              if ($target.is(":focus")) {
-                // Checking if the target was focused
-                return false;
-              } else {
-                $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
-                $target.focus(); // Set focus again
-              }
-            }
-          );
-        }
-      }
-    });
-
   $("#revealContent").hover(
     function () {
       $("body:not(.animate) .cta-section").addClass("hover");
@@ -81,30 +37,17 @@ jQuery(function ($) {
           ? "mousewheel"
           : "wheel";
         // Now bind the event to the desired element
-        var scroll = 0;
-        (function () {
-          $(document).on(wheelEvent, function (event) {
-            var oEvent = event.originalEvent,
-              delta2 = oEvent.deltaY || oEvent.wheelDelta;
-            if (delta2 > 0) {
-              scroll++;
-              if (scroll == 17) {
-                scroll = 0;
-              }
-              // deltaY for wheel event, wheelData for mousewheel event
-              if (scroll > 0 && scroll < 7) {
-                $("body").addClass("animate");
-                $(".cta-section").removeClass("hover");
-              } else if (scroll > 8 && $("body").hasClass("animate")) {
-                $(".cta-section").removeClass("hover");
-                $("body").removeClass("animate");
-              }
-            } else {
-              $("body").removeClass("animate");
-              scroll = 0;
-            }
-          });
-        })();
+        $("body").on(wheelEvent, function (e) {
+          var oEvent = e.originalEvent,
+            delta = oEvent.deltaY || oEvent.wheelDelta;
+          // deltaY for wheel event
+          // wheelData for mousewheel event
+          if (delta > 0) {
+            $("body").addClass("animate");
+          } else {
+            $("body").removeClass("animate");
+          }
+        });
       }
     })
     .resize();

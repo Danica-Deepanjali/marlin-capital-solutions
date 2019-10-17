e5897088d3e524ecf945c9884d59563b67a8013e
Danica Deepanjali <danica.deepanjali@ithands.biz>
2019-10-17 15:40:20 +0530

    new client change requests (17 oct)

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 jQuery(function ($) {
   $(document).ready(function () {
-    $("#getStarted,#revealContent,#subNav").click(function () {
+    $("#revealContent,#subNav").click(function () {
       $("body").toggleClass("animate");
       $(".cta-section").removeClass("hover");
     });
@@ -49,6 +49,13 @@ jQuery(function ($) {
           }
         });
       }
+
+      //if all tabs are closed in mobile view then made first tab active on resizing
+      if (!$(".tab-content > .tab-pane").hasClass("active")) {
+        $(".nav-tabs > a").removeClass("active");
+        $(".nav-tabs > a:first-child").addClass("active");
+        $(".tab-content > div:first-child").addClass("active");
+      }
     })
     .resize();
 
@@ -56,17 +63,14 @@ jQuery(function ($) {
   $(window)
     .on("resize", function () {
       if ($(window).width() < 992 || $(window).height() < 630) {
-        $(".tab-content .tab-pane").removeClass("fade");
+        $(".tab-content .tab-pane").removeClass("fade show");
         $(".tab-content .tab-pane").on("click", function () {
-          var container = $(this).parents(".tabbed-content"),
-            currId = $(this).attr("id"),
-            items = container.find(".tab-pane");
+          var container = $(this).parents(".tabbed-content");
+          currId = $(this).attr("id");
           container.find(".nav-tabs a").removeClass("active");
-          items.removeClass("active");
-          $(this).addClass("active");
-          container
-            .find('.nav-tabs a[href$="#' + currId + '"]')
-            .addClass("active");
+          container.find('.nav-tabs a[href$="#' + currId + '"]').toggleClass("active");
+          $(this).toggleClass("active");
+          $(this).siblings().removeClass("active");
         });
 
         // fix navbar on scroll in mobile devices
@@ -83,3 +87,9 @@ jQuery(function ($) {
     })
     .resize();
 });
+
+// to open link in new tab
+function openInNewTab(url) {
+  var win = window.open(url, "_blank");
+  win.focus();
+}
